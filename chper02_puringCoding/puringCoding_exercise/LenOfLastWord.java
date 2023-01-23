eccc9e595d2fdf315ee15a230975ef7084e3cb7f
zJanet <50511849+zJanet@users.noreply.github.com>
2023-01-23 21:50:41 +0800

    quicker way to find the last word's length

--- chper02_puringCoding/puringCoding_exercise/LenOfLastWord.java
+++ chper02_puringCoding/puringCoding_exercise/LenOfLastWord.java
@@ -8,9 +8,21 @@ public class LenOfLastWord {
         System.out.println(lengthOfLastWord(t1));
     }
     public static int lengthOfLastWord(String s) {
-        String[] splitedStr = s.split("\\s+");
-        System.out.println(Arrays.toString(splitedStr));
-        String lastStr = splitedStr[splitedStr.length-1];
-        return lastStr.length();
+        int orgLen = s.length();
+        int currentIndex = orgLen - 1;
+        while (currentIndex >= 0 && s.charAt(currentIndex) == ' '){
+            currentIndex--;
+        }
+        if (currentIndex < 0) return 0;
+        int lastWordLen = 0;
+        while(currentIndex >= 0 && s.charAt(currentIndex) != ' '){
+            lastWordLen++;
+            currentIndex--;
+        }
+        return lastWordLen;
+        // String[] splitedStr = s.split("\\s+");
+        // System.out.println(Arrays.toString(splitedStr));
+        // String lastStr = splitedStr[splitedStr.length-1];
+        // return lastStr.length();
     }
 }
